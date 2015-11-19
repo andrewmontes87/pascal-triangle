52b6db6c9447c675c10e7a812cc0a5e27b921dce
Andrew Montes <andrewmontes87@gmail.com>
2015-11-18 20:38:44 -0800

    coloring and positioning, and refactoring

--- pascal.js
+++ pascal.js
@@ -1,97 +1,115 @@
 
 function generateTriangle(totalLines) {
 
-	var Triangle = (function () {
-		function Triangle() {
+  var Triangle = (function () {
 
+    function Triangle() {
+      // public render function
+      this.render = function(matrix) {
+        // private renderRow function
+        var renderRow = function (row, index) {
+          // render and append
+          var template = "<p class='wrap' id='row" + String(index) + "'>";
+          for (var q = 0; q < row.length; q++) {
+            template += "&nbsp;<span class='label'>" + row[q] + "</span>&nbsp;";
+          }
+          template += "</p>";
+          $("#results").append(template);
 
+          // positioning
+          var containerWidth = $(".container").width();
+          var center = containerWidth / 2;
+          var rowWidth = $("#row" + index).width();
+          var xOffset = index * 1.5;
+          var yOffset = center - (rowWidth / 2);
+          $("#row" + index).css("top", String(xOffset)+"em")
+          $("#row" + index).css("left", String(yOffset)+"px")
 
-			this.prepMatrix = function() {
-				var matrix = [];
-				for (var line = 0; line < totalLines; line++) {
-					matrix[line] = [];
-					for (var element = 0; element <= line; element++) {
-						matrix[line][element] = 1;
-					}
-				}
-				return matrix;
-			};
+          // coloring
+          if (index % 2 === 0) {
+            // even row
+            var middle = Math.floor(row.length / 2) + 1;
+            $("#row" + index).children().addClass("label-success");
+            $("#row" + index).children( ":nth-child(" + middle + ")" ).addClass("label-danger");
 
-			this.calcValues = function(matrix) {
-				for (var line = 0; line < totalLines; line++) {
-					if (matrix[line].length > 2) {
-						for (var element = 0; element <= line; element++) {
-							if (element > 0 &&
-								element < line) {
-								
-								var leftParent = matrix[line-1][element-1];
-								var rightParent = matrix[line-1][element];
-								var value = leftParent + rightParent;
-								matrix[line][element] = value;
+          } else {
+            // odd row
+            var middleLeft = Math.floor(row.length / 2);
+            var middleRight = Math.floor(row.length / 2) + 1;
+            $("#row" + index).children().addClass("label-primary");
+            $("#row" + index).children( ":nth-child(" + middleLeft + ")" ).addClass("label-danger");
+            $("#row" + index).children( ":nth-child(" + middleRight + ")" ).addClass("label-danger");
+          }
+        }
 
-							}
-						}
-					}
-				}
-				return matrix;				
-			}
+        // init
+        $("#results").empty();
+        for (var i = 0; i < matrix.length; i++) {
+          renderRow(matrix[i], i);
+        }
+      }
 
+      // prep an matrix with the correct number of elements
+      // set all elements to 1
+      this.prepMatrix = function(matrix, totalLines) {
+        for (var line = 0; line < totalLines; line++) {
+          matrix[line] = [];
+          for (var element = 0; element <= line; element++) {
+            matrix[line][element] = 1;
+          }
+        }
+        return matrix;
+      };
 
-			this.matrix = [];
-			this.matrix = this.prepMatrix(totalLines);
-			this.matrix = this.calcValues(this.matrix);
+      // go through rows and apply pascal function
+      // top to bottom, look at parents to find value
+      this.calcValues = function(matrix) {
+        // for every row
+        for (var line = 0; line < totalLines; line++) {
+          // if it's not the first two rows, where elements remain 1
+          if (matrix[line].length > 2) {
+            // for every element
+            for (var element = 0; element <= line; element++) {
+              // if it's not the left-most or right-most element, which remain 1
+              if (element > 0 && element < line) {
+                // find left and right parents, sum for value
+                var leftParent = matrix[line-1][element-1];
+                var rightParent = matrix[line-1][element];
+                var value = leftParent + rightParent;
+                matrix[line][element] = value;
 
+              }
+            }
+          }
+        }
+        return matrix;        
+      };
 
-		}
-		return Triangle;
-	})();
+      // init
+      this.matrix = [];
+      this.matrix = this.prepMatrix(this.matrix, totalLines);
+      this.matrix = this.calcValues(this.matrix);
+      this.render(this.matrix);
+    }
 
-	return new Triangle();
+    return Triangle;
+  })();
+
+  return new Triangle();
 }
 
-
-
-var render = function(matrix) {
-	var renderRow = function (row) {
-
-		var template = "<p class='wrap'>";
-
-		for (var q = 0; q < row.length; q++) {
-			template += "<span class='badge'>" + row[q] + "</span>";
-		}
-
-		template += "</p>";
-
-		$("#results").append(template);
-	}
-
-	$("#results").empty();
-
-	for (var i = 0; i < matrix.length; i++) {
-		renderRow(matrix[i]);
-	}
-}
-
-
+// jquery form handler
 $("#form").submit(function( event ) {
-	event.preventDefault();
-
-	var input = $('#form :input');
-	input = input[0].value;
-	if (!isNaN(input)) {
-
-		var triangle = generateTriangle(input);
-		render(triangle.matrix);
-	
-
-		console.log(triangle.matrix);
-
-	} else {
-		//error
-		console.log('error!');
-	}
-
-
+  event.preventDefault();
+  // check input validity
+  var input = $('#form :input')[0].value;
+  if (!isNaN(input)) {
+    // generate the triangle
+    var triangle = generateTriangle(input);
+  } else {
+    //error
+    console.log('error!');
+  }
 });
 
 
